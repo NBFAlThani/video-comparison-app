dd2cb525c774366c16e686ec87e1a64cf37ff89e
Najla AlThani <nbf@macbook-pro.qf.org.qa>
2021-11-05 16:47:31 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request,send_from_directory, jsonify, url_for
+from flask import redirect
+
+# from scripts import helper
+import requests
+
+
+import json
+
+import time
+import os
+# import pickle
+
+application = Flask(__name__)
+
+@application.route('/home')
+def home():    
+    # return "hello"
+    return render_template("index.html")
+
+
+
+@application.route('/submit',methods=['GET','POST'])
+def submit():    
+    # return "hello"
+    print(request.form.keys())
+    first_name=request.form["fname"]
+    last_name=request.form["lname"]
+    print("values are",first_name,last_name)
+
+    # get the videos
+    # vid1=request.files["video1"]
+    # vid2=request.files["video2"]
+
+
+
+
+
+    # return "received files"
+    return first_name+" "+last_name
+
+
+
+
+
+if __name__ == "__main__":
+    
+    application.run(debug=True,port=8111)
